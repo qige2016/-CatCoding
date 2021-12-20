85eaa147c1543fba18261018a04e9e0383f5e142
qige2016 <286882998@qq.com>
2021-12-20 14:26:47 +0800

    add 01 Game Objects and Scripts

---
+++ basics/01 Game Objects and Scripts/Assets/Scripts/Clock.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+
+    [SerializeField]
+    Transform hoursPivot, minutesPivot, secondsPivot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation =
+            Quaternion.Euler(0f, 0f, hoursToDegrees * (float)time.TotalHours);
+        minutesPivot.localRotation =
+            Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
+        secondsPivot.localRotation =
+            Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
+    }
+}
